e53714a083672e8821d8a5dc1a4b8653290b8283
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2022-10-28 20:13:57 +0200

    build.sh: Adding -lm to fix linkage failure.

--- examples/build.sh
+++ examples/build.sh
@@ -1,7 +1,7 @@
 # The compilation for these examples should be very simple.
 # Something like in this file should work (if not exactly this).
-cc image.c -o image -O3 -I..
+cc image.c -o image -O3 -I.. -lm
 # These examples use SDL2 (https://libsdl.org/).
-cc cubes.c -o cubes -O3 -I.. `sdl2-config --cflags --libs`
-cc obj.c -o obj -O3 -I.. `sdl2-config --cflags --libs`
-cc fps.c -o fps -O3 -I.. `sdl2-config --cflags --libs`
+cc cubes.c -o cubes -O3 -I.. `sdl2-config --cflags --libs` -lm
+cc obj.c -o obj -O3 -I.. `sdl2-config --cflags --libs` -lm
+cc fps.c -o fps -O3 -I.. `sdl2-config --cflags --libs` -lm
